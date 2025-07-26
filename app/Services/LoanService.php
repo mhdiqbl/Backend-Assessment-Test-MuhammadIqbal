29159c57749b87f8e47e8a20639ccfadcebb57c7
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2025-07-26 16:42:57 +0700

    feat: repay loan

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -75,6 +75,76 @@ class LoanService
      */
     public function repayLoan(Loan $loan, int $amount, string $currencyCode, string $receivedAt): ReceivedRepayment
     {
-        //
+        DB::beginTransaction();
+        try {
+            $received = ReceivedRepayment::create([
+                'loan_id' => $loan->id,
+                'amount' => $amount,
+                'currency_code' => $currencyCode,
+                'received_at' => $receivedAt,
+            ]);
+
+            $repayments = $loan->scheduledRepayments()
+                ->whereIn('status', [
+                    ScheduledRepayment::STATUS_DUE,
+                    ScheduledRepayment::STATUS_PARTIAL,
+                ])
+                ->orderBy('due_date')
+                ->get();
+
+            $remainingAmount = $amount;
+
+            foreach ($repayments as $repayment) {
+                if ($remainingAmount <= 0) break;
+                $outstanding = $repayment->outstanding_amount;
+
+                if ($remainingAmount >= $outstanding) {
+                    // Repaid
+                    $repayment->update([
+                        'outstanding_amount' => 0,
+                        'status' => ScheduledRepayment::STATUS_REPAID,
+                    ]);
+                    $remainingAmount -= $outstanding;
+                } else {
+                    // Partial
+                    $repayment->update([
+                        'outstanding_amount' => $remainingAmount,
+                        'status' => ScheduledRepayment::STATUS_PARTIAL,
+                    ]);
+                    $remainingAmount = 0;
+                }
+            }
+
+            $amountReceipt = collect($loan->scheduledRepayments)
+                ->where('due_date', '<=', $receivedAt)
+                ->sum('amount');
+
+            $amountPartial = collect($loan->scheduledRepayments)
+                ->where('status','==','partial')
+                ->sum('outstanding_amount');
+
+            // Update the outstanding amount of the loan
+            $loan->outstanding_amount = max($loan->outstanding_amount - ($amountReceipt + $amountPartial), 0);
+
+            // Handling cases with outstanding balance of 1
+            if($loan->outstanding_amount === 1){
+                $loan->outstanding_amount = 0;
+            }
+
+            $loanStatus = $loan->outstanding_amount === 0
+                ? Loan::STATUS_REPAID
+                : Loan::STATUS_DUE;
+
+            $loan->update([
+                'outstanding_amount' => $loan->outstanding_amount,
+                'status' => $loanStatus,
+            ]);
+
+            DB::commit();
+            return $received;
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 }
