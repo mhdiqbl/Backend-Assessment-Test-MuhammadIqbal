e68b9869c837ab7255b660f535d685237bd251a4
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2025-07-26 13:23:04 +0700

    feat: createLoan

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -4,7 +4,10 @@ namespace App\Services;
 
 use App\Models\Loan;
 use App\Models\ReceivedRepayment;
+use App\Models\ScheduledRepayment;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LoanService
 {
@@ -21,7 +24,43 @@ class LoanService
      */
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
-        //
+        DB::beginTransaction();
+        try {
+            $loan = Loan::create([
+                'user_id' => $user->id,
+                'amount' => $amount,
+                'currency_code' => $currencyCode,
+                'terms' => $terms,
+                'outstanding_amount' => $amount,
+                'processed_at' => $processedAt,
+                'status' => Loan::STATUS_DUE,
+            ]);
+
+            $basic = intdiv($amount, $terms);
+            $remainder = $amount % $terms;
+
+
+            for ($i=1;$i <= $terms ;$i++) {
+                $dueDate = Carbon::parse($processedAt)->addMonth($i)->format('Y-m-d');
+
+                $installments = $basic + ($i === $terms ? $remainder : 0);
+
+                //create the scheduled repayment
+                $loan->scheduledRepayments()->create([
+                    'amount' => $installments,
+                    'outstanding_amount' => $installments,
+                    'currency_code' => $currencyCode,
+                    'due_date' => $dueDate,
+                    'status' => ScheduledRepayment::STATUS_DUE,
+                ]);
+            }
+
+            DB::commit();
+            return $loan;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 
     /**
